5fc3cec7ff6bfc5747b741d4a53ae6c292fe9d4e
Rayuga <tsarkar431@gmail.com>
2024-08-29 21:04:59 +0530

    portfolio

--- Misc/resize.py
+++ Misc/resize.py
@@ -1,9 +1,9 @@
 import cv2
 import os
 
-for (root,dirs,files) in os.walk("/home/rayuga/Downloads/256x256"):
+for (root,dirs,files) in os.walk("/home/rayuga/Downloads/icons/"):
     for f in files:
-        img=cv2.imread(root+'/'+f)
-        img=cv2.resize(img,(280,263))
-        cv2.imwrite(os.path.join("/home/rayuga/.config/neofetch/",f),img)
+        img=cv2.imread(root+f)
+        img=cv2.resize(img,(32,32))
+        cv2.imwrite(os.path.join("/home/rayuga/Documents/GitHub/Project/Portfolio/Theme1/assets/",f),img)
         
