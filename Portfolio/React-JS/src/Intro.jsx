bddfd79783c27f8178d6027ff5723c96f0ced36b
Rayuga2000 <tsarkar431@gmail.com>
2024-10-17 14:25:25 +0530

    react

--- Portfolio/React-JS/src/Intro.jsx
+++ Portfolio/React-JS/src/Intro.jsx
@@ -6,13 +6,15 @@ function Print() {
 function Intro() {
   return (
     <section>
-      <div className="card-intro flex flex-col-reverse md:flex-row justify-between">
+      <div className="card-intro flex items-center flex-col-reverse md:flex-row justify-between">
         <div id="intro">
           <p className="header-intro m-0">Hello, I'm Tushar<br />a Front-End Developer</p>
-          <pre className="mt-7 mb-4">I like bringing beautiful web designs into life<br />using the power of coding.</pre>
-          <button type="submit" onClick={Print}>Download CV</button>
+          <p className="mt-7 mb-4">I like bringing beautiful web designs into life using the power of coding. Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <button className="mt-4" type="submit" onClick={Print}>Download CV</button>
         </div>
-        <div className="profile-img grow"></div>
+        <div className="profile-img ">
+          <img src="/public/itadori.jpg" alt="" />
+        </div>
       </div>
     </section>
   )
